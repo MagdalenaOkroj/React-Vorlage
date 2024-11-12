22dc403ba0849120180203c634dd43903146bf82
Magdalena Okroj <magdalena.okroj@dci-student.org>
2024-11-12 12:57:14 +0100

    update3

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,7 +8,7 @@ const Content = (props) => {
             <h2>Welcome to My React App</h2>
             <p>Testing React.js with Vite</p>
             <img src="https://images.pexels.com/photos/907489/pexels-photo-907489.jpeg" alt="" />
-            {props.children}
+            {/* {props.children} */}
             {props.skills.map( (item,index) =>{
                 return <p key={index}>{item}</p>
             })}
